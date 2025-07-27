4a8fb55a45ef0bb41e9c86633b58a23d72a26be2
Ricardo Vargas <114964425+rfvargas@users.noreply.github.com>
2025-07-26 23:35:10 -0300

    Update script.js
    Somente foi incluído um código java script de um testes, mas o mesmo está comentado

--- js/script.js
+++ js/script.js
@@ -16,3 +16,31 @@ document.addEventListener('DOMContentLoaded', function() {
         cookieBanner.style.display = 'none'; // Esconde o banner
     });
 });
+
+
+/*
+// #########################################
+// ESTE SCRIPT SERVE PARA A CLASSE "social-link" PARA OS LINKS VOLTARREI AO PADRÃO NORMAL 
+// #########################################
+
+// Espera todo o conteúdo da página ser carregado para rodar o script
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Encontra todos os links que têm a classe 'social-link'
+    const socialLinks = document.querySelectorAll('.social-link');
+
+    // Para cada um desses links...
+    socialLinks.forEach(function(link) {
+
+      // Adiciona um "ouvinte" para o evento de clique
+      link.addEventListener('click', function() {
+        
+        // Usa um pequeno atraso para garantir que o navegador processe o clique
+        // e abra a nova aba antes de forçarmos a perda de foco.
+        setTimeout(function() {
+          link.blur(); // O comando mágico que força o link a perder o foco.
+        }, 100); // 100 milissegundos é um atraso imperceptível.
+      });
+    });
+});
+*/
